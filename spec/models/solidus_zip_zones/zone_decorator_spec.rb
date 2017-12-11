0f074aef901c69847f9c0b8699415ed2ddde627c
Daniele Palombo <daniele.palombo@gmail.com>
2017-12-11 16:27:45 +0100

    Add test for version lower than 2.4

--- spec/models/solidus_zip_zones/zone_decorator_spec.rb
+++ spec/models/solidus_zip_zones/zone_decorator_spec.rb
@@ -59,71 +59,95 @@ RSpec.describe SolidusZipZones::ZoneDecorator, type: :model do
     end
   end
 
-  context "#match" do
-    let(:country_zone) { create(:zone, name: 'CountryZone') }
-    let(:country) do
-      country = create(:country)
-      # Create at least one state for this country
-      create(:state, country: country)
-      country
-    end
-
-    before { country_zone.members.create(zoneable: country) }
-
-    context "when there is only one qualifying zone" do
-      let(:address) { create(:address, country: country, state: country.states.first) }
-
-      it "should return the qualifying zone" do
-        Spree::Deprecation.silence do
-          expect(Spree::Zone.match(address)).to eq(country_zone)
-        end
+  if SolidusSupport.solidus_gem_version < Gem::Version.new('2.4')
+    context "#match" do
+      let(:country_zone) { create(:zone, name: 'CountryZone') }
+      let(:country) do
+        country = create(:country)
+        # Create at least one state for this country
+        create(:state, country: country)
+        country
       end
-    end
 
-    context "when there are two qualified zones with same member type" do
-      let(:address) { create(:address, country: country, state: country.states.first) }
-      let(:second_zone) { create(:zone, name: 'SecondZone') }
+      before { country_zone.members.create(zoneable: country) }
 
-      before { second_zone.members.create(zoneable: country) }
+      context "when there is only one qualifying zone" do
+        let(:address) { create(:address, country: country, state: country.states.first) }
 
-      context "when both zones have the same number of members" do
-        it "should return the zone that was created first" do
+        it "should return the qualifying zone" do
           Spree::Deprecation.silence do
             expect(Spree::Zone.match(address)).to eq(country_zone)
           end
         end
       end
 
-      context "when one of the zones has fewer members" do
-        let(:country2) { create(:country) }
+      context "when there are two qualified zones with same member type" do
+        let(:address) { create(:address, country: country, state: country.states.first) }
+        let(:second_zone) { create(:zone, name: 'SecondZone') }
 
-        before { country_zone.members.create(zoneable: country2) }
+        before { second_zone.members.create(zoneable: country) }
 
-        it "should return the zone with fewer members" do
-          Spree::Deprecation.silence do
-            expect(Spree::Zone.match(address)).to eq(second_zone)
+        context "when both zones have the same number of members" do
+          it "should return the zone that was created first" do
+            Spree::Deprecation.silence do
+              expect(Spree::Zone.match(address)).to eq(country_zone)
+            end
+          end
+        end
+
+        context "when one of the zones has fewer members" do
+          let(:country2) { create(:country) }
+
+          before { country_zone.members.create(zoneable: country2) }
+
+          it "should return the zone with fewer members" do
+            Spree::Deprecation.silence do
+              expect(Spree::Zone.match(address)).to eq(second_zone)
+            end
           end
         end
       end
-    end
 
-    context "when there are two qualified zones with different member types" do
-      let(:state_zone) { create(:zone, name: 'StateZone') }
-      let(:address) { create(:address, country: country, state: country.states.first) }
+      context "when there are two qualified zones with different member types" do
+        let(:state_zone) { create(:zone, name: 'StateZone') }
+        let(:address) { create(:address, country: country, state: country.states.first) }
 
-      before { state_zone.members.create(zoneable: country.states.first) }
+        before { state_zone.members.create(zoneable: country.states.first) }
 
-      it "should return the zone with the more specific member type" do
-        Spree::Deprecation.silence do
-          expect(Spree::Zone.match(address)).to eq(state_zone)
+        it "should return the zone with the more specific member type" do
+          Spree::Deprecation.silence do
+            expect(Spree::Zone.match(address)).to eq(state_zone)
+          end
         end
       end
-    end
 
-    context "when there are no qualifying zones" do
-      it "should return nil" do
-        Spree::Deprecation.silence do
-          expect(Spree::Zone.match(Spree::Address.new)).to be_nil
+      context "when there are three qualified zones with different member types" do
+        let!(:zip_zone)   { create(:zone, name: 'ZipZone', zipcodes: '12345,12346') }
+
+        let(:state_zone) { create(:zone, name: 'StateZone') }
+        let(:address) do
+          create(:address,
+                 country: country,
+                 state: country.states.first,
+                 zipcode: '12345')
+        end
+
+        before do
+          state_zone.members.create(zoneable: country.states.first)
+        end
+
+        it "should return the zone with the more specific member type" do
+          Spree::Deprecation.silence do
+            expect(Spree::Zone.match(address)).to eq(zip_zone)
+          end
+        end
+      end
+
+      context "when there are no qualifying zones" do
+        it "should return nil" do
+          Spree::Deprecation.silence do
+            expect(Spree::Zone.match(Spree::Address.new)).to be_nil
+          end
         end
       end
     end
