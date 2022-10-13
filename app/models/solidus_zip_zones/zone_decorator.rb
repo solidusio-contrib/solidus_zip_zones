5783893abe2456615ac743c412a040099ddce87a
embold-given <given@embold.com>
2022-10-13 08:52:36 -0400

    Add support for Zip+4 in US Zip codes

--- app/models/solidus_zip_zones/zone_decorator.rb
+++ app/models/solidus_zip_zones/zone_decorator.rb
@@ -7,6 +7,9 @@ module SolidusZipZones
         if state_ids.blank? && country_ids.blank? && zipcode.blank?
           none
         else
+          if country_ids==233
+            zipcode = zipcode.split('-').first
+          end
           spree_zone_members_table = Spree::ZoneMember.arel_table
           matching_state =
             spree_zone_members_table[:zoneable_type].eq("Spree::State").
